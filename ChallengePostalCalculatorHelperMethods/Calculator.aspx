3962dc86bc76259087d1ffc0121d5aae7d87a272
Kimberly Davis <kwatsondavis@gmail.com>
2015-09-29 10:04:03 -0400

    Initial Commit

---
+++ ChallengePostalCalculatorHelperMethods/Calculator.aspx
@@ -0,0 +1,36 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="Calculator.aspx.cs" Inherits="ChallengePostalCalculatorHelperMethods.Calculator" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+    <div>
+    
+        Postal Calculator<br />
+        <br />
+        Width:
+        <asp:TextBox ID="widthTextBox" runat="server" AutoPostBack="True"></asp:TextBox>
+        <br />
+        Height: <asp:TextBox ID="heightTextBox" runat="server" AutoPostBack="True"></asp:TextBox>
+        <br />
+        Length:
+        <asp:TextBox ID="lengthTextBox" runat="server" AutoPostBack="True"></asp:TextBox>
+        <br />
+        <br />
+        <asp:RadioButton ID="groundRadioButton" runat="server" AutoPostBack="True" GroupName="PostalGroup" OnCheckedChanged="groundShipping_CheckedChanged" Text="Ground" />
+        <br />
+        <asp:RadioButton ID="airRadioButton" runat="server" AutoPostBack="True" GroupName="PostalGroup" OnCheckedChanged="airShipping_CheckedChanged" Text="Air" />
+        <br />
+        <asp:RadioButton ID="nextDayAirRadioButton" runat="server" AutoPostBack="True" GroupName="PostalGroup" OnCheckedChanged="nextDayShipping_CheckedChanged" Text="Next Day Air" />
+        <br />
+        <br />
+        <asp:Label ID="resultLabel" runat="server"></asp:Label>
+    
+    </div>
+    </form>
+</body>
+</html>
---
+++ ChallengePostalCalculatorHelperMethods/Calculator.aspx.cs
@@ -0,0 +1,118 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace ChallengePostalCalculatorHelperMethods
+{
+    public partial class Calculator : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void groundShipping_CheckedChanged(object sender, EventArgs e)
+        {
+            calculateGroundShipping();
+        }
+
+        protected void airShipping_CheckedChanged(object sender, EventArgs e)
+        {
+            calculateAirShipping();
+        }
+
+        protected void nextDayShipping_CheckedChanged(object sender, EventArgs e)
+        {
+            calculateNextDayShipping();
+        }
+
+        protected void calculateGroundShipping()
+        {
+            double width = 0;
+            double height = 0;
+            double length = 0;
+
+            if (widthTextBox.Text.Trim().Length == 0) return;
+            if (heightTextBox.Text.Trim().Length == 0) return;
+            if (lengthTextBox.Text.Trim().Length == 0) length = 1;
+
+            if (!Double.TryParse(widthTextBox.Text, out width))
+                return;
+            if (!Double.TryParse(heightTextBox.Text, out height))
+                return;
+            if (!Double.TryParse(lengthTextBox.Text, out length))
+            {
+                if (length == 0)
+                {
+                    length = 1;
+                }
+            }
+            
+            double volume = width * height * length;
+            double groundTotal = volume * 0.15;
+
+            resultLabel.Text = String.Format("Your parcel will cost {0:C} to ship.", groundTotal);
+
+        }
+
+        protected void calculateAirShipping()
+        {
+            double width = 0;
+            double height = 0;
+            double length = 0;
+            
+            if (widthTextBox.Text.Trim().Length == 0) return;
+            if (heightTextBox.Text.Trim().Length == 0) return;
+            if (lengthTextBox.Text.Trim().Length == 0) length = 1;
+
+            if (!Double.TryParse(widthTextBox.Text, out width))
+                return;
+            if (!Double.TryParse(heightTextBox.Text, out height))
+                return;
+            if (!Double.TryParse(lengthTextBox.Text, out length))
+            {
+                if (length == 0)
+                {
+                    length = 1;
+                }
+            }
+                
+            double volume = width * height * length;
+            double airTotal = volume * 0.25;
+
+            resultLabel.Text = String.Format("Your parcel will cost {0:C} to ship.", airTotal);
+
+        }
+
+        protected void calculateNextDayShipping()
+        {
+            double width = 0;
+            double height = 0;
+            double length = 0;
+            
+            if (widthTextBox.Text.Trim().Length == 0) return;
+            if (heightTextBox.Text.Trim().Length == 0) return;
+            if (lengthTextBox.Text.Trim().Length == 0) length = 1;
+
+            if (!Double.TryParse(widthTextBox.Text, out width))
+                return;
+            if (!Double.TryParse(heightTextBox.Text, out height))
+                return;
+            if (!Double.TryParse(lengthTextBox.Text, out length))
+            {
+                if (length == 0)
+                {
+                    length = 1;
+                }
+            }
+
+            double volume = width * height * length;
+            double nextDayTotal = volume * 0.45;
+
+            resultLabel.Text = String.Format("Your parcel will cost {0:C} to ship.", nextDayTotal);
+        }
+    }
+}
